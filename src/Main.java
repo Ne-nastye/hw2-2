01ab478dbd3c5f5c57b7f80ebb188ad5975e722b
Nastasya <Nenastye90@gmail.com>
2023-06-16 22:30:54 +0300

    bug fix

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,16 @@ public class Main {
     public static void main(String[] args) {
 
         int balance = 100; // начальный баланс денежных средств на телефоне
-        int drop = 400; // сумма пополнения
+        int drop = 1100; // сумма пополнения
         int totalbalance = balance + drop; // итоговый баланс денежных средств на телефоне
-        int bonus = drop / 100 * 10; // бонус при пополнении на сумму от 500р и более
-        if (drop >= 500) {
+        int bonus = drop / 100; // бонус: при пополнении на сумму от 1000р за каждые 100р будет бонус=1р
+        if (drop >= 1000) {
             System.out.println("Итоговый баланс счета: " + (totalbalance + bonus));
         } else {
             System.out.println("Итоговый баланс счета: " + totalbalance);
         }
-        if (drop >= 500) {
+        if (drop >= 1000) {
             System.out.println("Бонусы: " + bonus);
         }
     }
+}
